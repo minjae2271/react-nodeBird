642b2904e4316d8a9cfb06fcc2609dc9b47c863c
minjae <minjae2271@naver.com>
2023-10-17 23:01:06 +0900

    comment form

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -1,10 +1,11 @@
 import React, { useCallback, useState } from "react";
 import propTypes from 'prop-types';
 import { useSelector } from "react-redux";
-import { Avatar, Button, Card, Popover} from 'antd';
+import { Avatar, Button, Card, Popover, List, Comment} from 'antd';
 import { EllipsisOutlined, HeartOutlined, MessageOutlined, RetweetOutlined } from "@ant-design/icons";
 
 import PostImages from "./PostImages";
+import CommentForm from "./CommentForm";
 
 const PostCard = ({ post }) => {
     const id = useSelector((state) => state.user.me?.id);
@@ -44,7 +45,25 @@ const PostCard = ({ post }) => {
                     description={post.content}
                 />
             </Card>
-            { commentFormOpen && <div>댓글</div>}
+            { commentFormOpen && (
+                <div>
+                    <CommentForm post={post}/>
+                    <List 
+                        header={`${post.Comments.length}개의 댓글`}
+                        itemLayout="horizontal"
+                        dataSource={post.Comments}
+                        renderItem={(item) => (
+                            <li>
+                                <Comment
+                                    author={item.User.nickname}
+                                    avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+                                    content={item.content}
+                                />
+                            </li>
+                        )}
+                    />
+                </div>
+            )}
             {/* <CommentForm />
             <Comments /> */}
         </div>
