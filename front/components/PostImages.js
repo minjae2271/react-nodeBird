b2f11a6ca8a410958c1a55c890eee2a73d0e76b6
minjae <minjae2271@naver.com>
2023-10-23 23:14:50 +0900

    postImages

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import propTypes from 'prop-types';
+import { PlusOutLined } from 'antd';
 
 const PostImages = ({ images }) => {
+    const [showImagesZoom, setShowImagesZoom] = useState(false);
+    const onZoom = useCallback(() => {
+        setShowImagesZoom(true);
+    }, []);
+
+    if (images.length === 1) {
+        return (
+            <>
+                <img src={images[0].src} onClick={onZoom} />
+            </>
+        )
+    }
+    if (images.length === 2) {
+        return (
+            <>
+                <img src={images[0].src} style={{ width:'50%', display: 'inline'}} onClick={onZoom} />   
+                <img src={images[0].src} style={{ width:'50%', display: 'inline'}} onClick={onZoom} />  
+            </>
+        )
+    }
+
     return (
-        <div>구현중...</div>
+        <>
+            <div>
+                <img src={images[0].src} onClick={onZoom} />
+                <div
+                    style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle'}}
+                    onClick={onZoom}
+                >
+                    <PlusOutLined />
+                    <br />
+                    {images.length}
+                    개의 사진 더보기
+                </div> 
+            </div>
+        </>
     )
 }
 
